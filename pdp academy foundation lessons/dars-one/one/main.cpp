85192069cc2986c170c59c2afcbb7ae9e1f4c291
MuhammadYusuf0966 <yusufabdugofurov73@gmail.com>
2023-06-04 19:39:27 +0500

    initial commit

---
+++ pdp academy foundation lessons/dars-one/one/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout << "               /\\                " << endl;
+        cout << "              /  \\               " << endl;
+        cout << "             /    \\              " << endl;
+        cout << "            /______\\             " << endl;
+        cout << "            |      |              " << endl;
+        cout << "            |      |              " << endl;
+        cout << "            |      |              " << endl;
+        cout << "            |______|              " << endl;
+        cout << "                                  " << endl;
+
+
+
+
+
+
+
+        cout << "             C C                                     " << endl;
+        cout << "           C     C          +            +           " << endl;
+        cout << "           C                +            +           " << endl;
+        cout << "           C            + + + + +    + + + + +       " << endl;
+        cout << "           C                +            +           " << endl;
+        cout << "           C                +            +           " << endl;
+        cout << "           C     C                                   " << endl;
+        cout << "           C C C                                     " << endl;
+        cout << "                                                     " << endl;
+
+
+
+
+        cout << "          _____________________________________________________                                                                                                                  " << endl;
+        cout << "         |                |         |                    |     |          " << endl;
+        cout << "         | COUNTRY        | NAME    | PROFESSION         | AGE |          " << endl;
+        cout << "         |________________|_________|____________________|_____|          " << endl;
+        cout << "         |                |         |                    |     |          " << endl;
+        cout << "         | GERMANY        | MICHAEL | COMPYUTER INGENEER | 19  |          " << endl;
+        cout << "         |________________|_________|____________________|_____|          " << endl;
+        cout << "         |                |         |                    |     |          " << endl;
+        cout << "         | ENGLAND        | ROBERT  | ARTIST             | 34  |          " << endl;
+        cout << "         |________________|_________|____________________|_____|          " << endl;
+        cout << "         |                |         |                    |     |          " << endl;
+        cout << "         | UNITED KINGDOM | JULIA   | INDOOR DESIGNER    | 27  |          " << endl;
+        cout << "         |________________|_________|____________________|_____|          " << endl;
+        cout << "         |                |         |                    |     |          " << endl;
+        cout << "         | UNITED STAATES | JO      | ACTOR              | 21  |          " << endl;
+        cout << "         |________________|_________|____________________|_____|          " << endl;
+        cout << "                                                                          " << endl;
+
+
+
+
+
+        cout << "           +\"\"\"\"\"\"\+     " << endl;
+        cout << "          [| O O  |]    " << endl;
+        cout << "           |  ^   |     " << endl;
+        cout << "           | '-'  |     " << endl;
+        cout << "           +------+     " << endl;
+        cout << "                        " << endl;
+
+
+
+
+
+
+
+
+        cout << "                                               ******************************                                                       " << endl;
+        cout << "                                             * *                          * *                                                       " << endl;
+        cout << "                                           *   *                        *   *                                                             " << endl;
+        cout << "                                        *      *                      *     *                                                           " << endl;
+        cout << "                                      *        *                    *       *                                                             " << endl;
+        cout << "                                    *          *                  *         *                                                            " << endl;
+        cout << "                                  * ******************************          *                                                   " << endl;
+        cout << "                                  *            *                 *          *                                                           " << endl;
+        cout << "                                  *            *                 *          *                                                            " << endl;
+        cout << "                                  *            *                 *          *                                                             " << endl;
+        cout << "                                  *            *                 *          *                                                               " << endl;
+        cout << "                                  *            ******************************                                                                                           " << endl;
+        cout << "                                  *          *                   *          *                                                                 " << endl;
+        cout << "                                  *        *                     *        *                                                                    " << endl;
+        cout << "                                  *      *                       *      *                                                                  " << endl;
+        cout << "                                  *    *                         *    *                                                                         " << endl;
+        cout << "                                  * *                            * *                                                                          " << endl;
+        cout << "                                  *********************************                                                                                                              " << endl;
+        cout << "                                                                                                                                            " << endl;
+        cout << "                                                                                                                                             " << endl;
+        cout << "                                                                                                                                            " << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
